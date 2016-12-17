54c871076f09887f0abed402913fec8288508170
Nicholas Litwin <nicklitwin88@gmail.com>
2016-12-16 21:12:26 -0800

    Remove yet-to-be-implemented tests in favor of CalcValue tests

--- src/App.test.js
+++ src/App.test.js
@@ -6,11 +6,3 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
-
-it('adds two numbers together', () => {
-
-})
-
-it ('handles computing new results when = is pressed more than once in a row', () => {
-
-})
