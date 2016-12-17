eca4b03f372d5dcc8520fd3a310e5c69fb286267
Nicholas Litwin <nicklitwin88@gmail.com>
2016-12-16 17:39:17 -0800

    Remove boilerplate

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import CalcInput from './Components/CalcInput'
+import ButtonOperators from './Components/ButtonOperators'
+import ButtonValues from './Components/ButtonValues'
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      lastOperator: ''
+      lastValue: 0
+    }
+  }
+
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <CalcInput />
+
+        <ButtonOperators />
+
+        <ButtonValues />
       </div>
     );
   }
